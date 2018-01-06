9cb066d354bf828e224fd9bf989605788b681f0f
yosuke adachi <doruji1982@gmail.com>
2018-01-06 14:05:56 +0900

    calculate

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,9 +1,23 @@
 
-# no declare
+# 型の宣言は不要
 val = 100
 
+# 代入演算可
 val += 100
 
-if (val > 100):
-	print("val is over 100")
-	print(val)
+# 切り捨て除算
+val = 10 // 3
+print(val)
+
+# 剰余
+val = 10 % 3
+print(val)
+
+# 3項演算
+print('ok' if val > 0 else 'ng')
+
+# 条件分岐
+# オブジェクトの等価は is で判定する
+#if (val > 100):
+#	print("val is over 100")
+#	print(val)
