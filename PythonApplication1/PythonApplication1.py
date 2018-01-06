3633d0e7bfe3913353f6f09cbe8c615e8408a4d3
yosuke adachi <doruji1982@gmail.com>
2018-01-06 14:21:24 +0900

    decimal

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,7 +3,9 @@
 val = 100
 
 # 代入演算可
+# printでconsole出力
 val += 100
+print(val)
 
 # 切り捨て除算
 val = 10 // 3
@@ -13,8 +15,14 @@ print(val)
 val = 10 % 3
 print(val)
 
+# 小数はfloat
+val = 1 + 1.0
+print(val)
+
 # 3項演算
-print('ok' if val > 0 else 'ng')
+# 文字列はsingle quoteでくくる
+# indexで文字列の切り出し可能
+print('ok'[:1] if val > 0 else 'ng'[1:])
 
 # 条件分岐
 # オブジェクトの等価は is で判定する
