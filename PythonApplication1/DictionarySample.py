14e4f0ccda80d5a82d8c02e954a2634b4d9a9fb2
yosuke adachi <doruji1982@gmail.com>
2018-01-07 23:20:58 +0900

    dictionary advanced

--- PythonApplication1/DictionarySample.py
+++ PythonApplication1/DictionarySample.py
@@ -11,3 +11,17 @@ dict = {"k1":"v1"
 
 print(dict["k2"])
 print(dict[1])
+
+dict1 = {"apple":"red", "grape":"purple", "banana":"yellow"}
+# dict object is not callable.
+# dict2 = dict(one=1, two=2, three=3)
+
+print("apple" in dict1)
+# print("zero" in dict2)
+
+for key, val in dict1.items():
+	print(key, val)
+
+for key in dict1.keys():
+	print(dict1[key])
+
