feeaa3b7bf122d68868a749b29d9c7e8d0b14f91
yosuke adachi <doruji1982@gmail.com>
2018-01-07 14:55:28 +0900

    exception handling

---
+++ PythonApplication1/ExceptionSample.py
@@ -0,0 +1,16 @@
+# exception
+
+try:
+	x = 10 / 0
+except Exception as e:
+	print(e) # division by zero
+else:
+	# exception does not raise
+	print("any exceptions had not raised.")
+finally:
+	print("finally fuga") # fuga
+
+try:
+	raise Exception("hoge")
+except Exception as e:
+	print(e) # hoge
