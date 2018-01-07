ca376db01f1fb496638d5abea27c9b63b6be92ed
yosuke adachi <doruji1982@gmail.com>
2018-01-07 23:35:50 +0900

    tuple advanced

--- PythonApplication1/TupleSample.py
+++ PythonApplication1/TupleSample.py
@@ -7,3 +7,19 @@ print(t1[1])
 
 t2 = ("fuga",)
 print(t2[0])
+
+key1 = ("masaru", "10-18")
+key2 = ("manabu", "05-01")
+
+dict = {key1:38, key2:25}
+print(dict)
+
+# return multiple value
+def getABC():
+	return ("A", "B", "C")
+
+(a,b,c) = getABC()
+
+print(a)
+print(b)
+print(c)
